4b79d1094763fac3a6e74b1b5575efcbb19b4f6b
Oleg Klyuev <olegkluev1507@gmail.com>
2023-04-13 21:39:34 +0500

    feat: переписал наименования переменных. Разобрался с проблемой аргумента/параметра

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,50 @@
-let editProfileButtons = document.querySelector('.profile__button-edit');
-let popup = document.querySelector('.popup');
-let closePopupButton = popup.querySelector('.popup__button-close');
-let userAuthorProfile = document.querySelector('.profile__author');
-let userJobProfile = document.querySelector('.profile__job');
-let userAuthorInput = popup.querySelector('.popup__input_type_author'); 
-let userJobInput = popup.querySelector('.popup__input_type_job');
-let popupForm = popup.querySelector('.popup__form');
-let saveFormButton = popup.querySelector('.popup__button-save')
+// Объявление всех переменных
 
+// Объявление данных profile
+let userAuthorProfile = document.querySelector('.profile__author')
+let userJobProfile = document.querySelector('.profile__job')
+let buttonEditProfile = document.querySelector('.profile__button-edit')
 let buttonAddAuthor = document.querySelector('.profile__add-button')
+
+// Объявление данных попапа editProfile
+let popupEditProfile = document.querySelector('.popup-edit-profile')
+let popupEditProfileForm = popupEditProfile.querySelector('.popup__form[name=profile]')
+let userAuthorInput = popupEditProfile.querySelector('.popup__input[name=author]')
+let userJobInput = popupEditProfile.querySelector('.popup__input[name=job]')
+let popupEditProfileButtonSaveForm = popupEditProfile.querySelector('.popup__button-save')
+let popupEditProfileButtonClose = popupEditProfile.querySelector('.popup__button-close_type_edit-profile')
+
+// Объявление данных попапа addAuthor
 let popupAddAuthor = document.querySelector('.popup-add-author')
+let popupFormAddAuthor = popupAddAuthor.querySelector('.popup__form[name=place]')
+let popupAddAuthorButtonSaveForm = popupEditProfile.querySelector('.popup__button-create')
+let popupAddAuthorButtonClose = popupAddAuthor.querySelector('.popup__button-close_type_add-author')
+
 
 
 // Открытие попапа 'Редактировать'
 function editProfile() {
-  userAuthorInput.value = userAuthorProfile.textContent;
-  userJobInput.value = userJobProfile.textContent; 
-  popup.classList.add('popup_opened');
+  userAuthorInput.value = userAuthorProfile.textContent
+  userJobInput.value = userJobProfile.textContent 
+  popupEditProfile.classList.add('popup_opened')
 }
 
 // Закрытие попапа 'Редактировать'
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function closePopupEditProfile() {
+  popupEditProfile.classList.remove('popup_opened')
 }
 
 function addAuthor() {
-  popupAddAuthor.classList.add('popup_opened');
+  popupAddAuthor.classList.add('popup_opened')
 }
 
+// function closeAuthor() {
+//   popupAddAuthor.classList.remove('popup_opened')
+// }
 
-editProfileButtons.addEventListener('click', editProfile);
-closePopupButton.addEventListener('click', closePopup);
+
+buttonEditProfile.addEventListener('click', editProfile)
+popupEditProfileButtonClose.addEventListener('click', closePopupEditProfile)
 buttonAddAuthor.addEventListener('click', addAuthor)
 
 
@@ -39,10 +53,10 @@ function saveProfile(evt) {
   evt.preventDefault();
   userAuthorProfile.textContent = userAuthorInput.value;
   userJobProfile.textContent = userJobInput.value;
-  closePopup(popup);
+  closePopupEditProfile(popupEditProfile);
 }
 
-popupForm.addEventListener('submit', saveProfile);
+popupEditProfileForm.addEventListener('submit', saveProfile);
 
 
 const initialCards = [
